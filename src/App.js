89e246611016a32d7058d149d14dc0c1e0899109
shubhamkanse72 <shubhamkanse7272@gmail.com>
2024-03-04 21:18:22 +0530

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Player from "./components/Player";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player name='Player 1' symbol='X' />
+          <Player name='Player 2' symbol='O' />
+        </ol>
+        Game Board
+      </div>
+    </main>
   );
 }
 
